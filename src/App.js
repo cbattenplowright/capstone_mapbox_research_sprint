8788b55cd52c0ad799abff476079f9872f0f1d31
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-20 14:51:39 +0100

    added mapboxgl import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken =
+    "pk.eyJ1IjoiY2JhdHRlbnBsb3dyaWdodCIsImEiOiJjbG1ybHg0YXowMXNxMm1xY2hmNHFhN243In0.9oNc1GPQFf9qqxck4IJMnA";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <></>;
 }
 
 export default App;
