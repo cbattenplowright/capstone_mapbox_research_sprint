d45a8404929e7614013993963ee2c920c6223712
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-20 23:26:02 +0100

    added mock orders JSON file

---
+++ src/ordersJSON.js
@@ -0,0 +1,22 @@
+export const orders = [
+  {
+    id: 1,
+    coordinates: [-84.5215, 39.1085],
+    items: 4
+  },
+  {
+    id: 2,
+    coordinates: [-84.5059, 39.1327],
+    items: 6
+  },
+  {
+    id: 3,
+    coordinates: [-84.5314, 39.1505],
+    items: 2
+  },
+  {
+    id: 4,
+    coordinates: [-84.5424, 39.1373],
+    items: 8
+  }
+];
